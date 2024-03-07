3028d9f6d0c34b0a99b8051366f657d2bd071e49
firyalshafa <shafafiryal782@gmail.com>
2024-03-07 15:38:40 +0700

    membuat prosedur input

--- Pertemuan ke 3/Pertemuan ke 3.cpp
+++ Pertemuan ke 3/Pertemuan ke 3.cpp
@@ -7,6 +7,29 @@ using namespace std;
 int arr[20];
 int n;
 
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+
+        }
+            
+    }
+    cout << endl;
+    cout << "=============" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "==============" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << " : ";
+        cin >> arr[i];
+
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
